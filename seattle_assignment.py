25638d0657c9678224144730134c3d5e10782804
Barend Nooren <barend.nooren@gmail.com>
2026-01-13 16:50:56 +0100

    Most of part3

--- seattle_assignment.py
+++ seattle_assignment.py
@@ -7,65 +7,77 @@ import csv
 with open('precipitation.json', encoding='utf-8') as file:
     data_precipitation = json.load(file)
 
-#filter for seattle measurements
-seattle_station = 'GHCND:US1WAKG0038'
-seattle_measurements = []
-    
-for entry in data_precipitation:
-    if entry['station'] == seattle_station:
-        seattle_measurements.append(entry)
+#Order data per station
+stations = []
+with open('stations.csv', encoding='utf-8') as f:
+    for line in f:
+        station_strings = line.strip().split(',')
+        stations.append(station_strings)
 
-# print(f'In Seattle, {len(seattle_measurements)} have been taken')
+#process all stations
+all_results = {}
+data_rows = stations[1:]
 
-#filter for seattle measurements per month
-total_monthly = {}
+for row in data_rows:
+    location = row[0]
+    state = row[1]
+    station_code = row[2]
 
-for measurement in seattle_measurements:
-    date_string = measurement['date']
-    date_parts = date_string.split('-')
-    month = int(date_parts[1]) #turning month notation into integers
+    station_measurements = []
+    for entry in data_precipitation:
+        if entry['station'] == station_code:
+            station_measurements.append(entry)
 
-    precip = measurement['value']
+#Obtain totals per month per stations
+    total_monthly = {}
 
-    if month in  total_monthly:
-        total_monthly[month] = total_monthly[month] + precip
+    for measurement in station_measurements:
+        date_string = measurement['date']
+        date_parts = date_string.split('-')
+        month = int(date_parts[1]) #turning month notation into integers
+
+        precip = measurement['value']
+
+        if month in total_monthly:
+            total_monthly[month] = total_monthly[month] + precip
+            
+        else:
+            total_monthly[month] = precip
         
-    else:
-        total_monthly[month] = precip
-    
 
-# print(f'Monthly total precipitation: {total_monthly}')
+    # print(f'Monthly total precipitation: {total_monthly}')
 
-#Calculating yearly precipitation
-total_yearly_precipitation = sum(total_monthly.values())
+    # #Calculating yearly precipitation
+    total_yearly_precipitation = sum(total_monthly.values())
 
-#Calculating relative monthly precipitation
-relative_monthly_precipitation = {}
+    #Calculating relative monthly precipitation
+    relative_monthly_precipitation = {}
 
-for month, monthly_total in total_monthly.items(): #Create pairs of (month, monthly total) 
+    for month, monthly_total in total_monthly.items(): #Create pairs of (month, monthly total) 
     #get proportion for relative precipitation
-    proportion = monthly_total/total_yearly_precipitation
+        proportion = monthly_total/total_yearly_precipitation
 
-    if month in relative_monthly_precipitation:
-        relative_monthly_precipitation[month] = relative_monthly_precipitation[month] + proportion
+        if month in relative_monthly_precipitation:
+            relative_monthly_precipitation[month] = relative_monthly_precipitation[month] + proportion
 
-    else:
-        relative_monthly_precipitation[month] = proportion
+        else:
+            relative_monthly_precipitation[month] = proportion
 
-        
-
+    #calculate relative yearly precipitation
+    total_all_stations = 0
+    for perlocation_data in all_results.
 
 #dumping results in json file
-data = {
-            "Seattle": {
-    "station": 'GHCND:US1WAKG0038',
-    "state": "WA",
-    "total_monthly_precipitation": [total_monthly],
-    "total_yearly_precipitation": "11180"
-    "relative_monthly_precipitation" [relative_monthly_precipitation]
+    data = {
+            [location]: {
+        "station": [station_code],
+        "state": [state],
+        "total_monthly_precipitation": [total_monthly],
+        "total_yearly_precipitation": "11180",
+        "relative_monthly_precipitation": [relative_monthly_precipitation]
+            }
+
     }
 
-}
-
-with open('results.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, indent=4, ensure_ascii=False)
+    with open('results.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
